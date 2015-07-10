97c44bb479afdd365ecc201a600b57b9844cc76d
Kirill Kondratenko <cybem@cybem.info>
2015-07-11 00:34:22 +0300

    Version 0.10.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject whisper2cyanite "0.9.1-SNAPSHOT"
+(defproject whisper2cyanite "0.10.0-SNAPSHOT"
   :description "Whisper to Cyanite data migration tool."
   :url "https://github.com/cybem/whisper2cyanite"
   :license {:name "MIT License"
